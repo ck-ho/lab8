4ad050f7a8767f732cddf3d7ce3abaac884fe8c2
ck-ho <>
2021-02-25 09:59:19 -0800

    thirdpartAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = "JrrtXEFqufokr0QChSslHGtljN4FZxQd";
+
+  var map = L.mapquest.map('map', {
+    center: [32.878590, -117.235829],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([32.878590, -117.235829]).addTo(map);
 }
